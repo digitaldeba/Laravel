3e0b0ddb151b86cdfc8a1461f87056bfcb791127
digitaldeba <129753464+digitaldeba@users.noreply.github.com>
2024-01-30 00:36:46 +0530

    Update 5Route.php

--- blog/5Route.php
+++ blog/5Route.php
@@ -1,10 +1,11 @@
 <?php
-https://github.com/digitaldeba/Laravel/blob/master/blog/3InstallLaravelAnd.html
-
-blog\routes\web.php
--------------------
-get:
-----
+// https://github.com/digitaldeba/Laravel/blob/master/blog/3InstallLaravelAnd.html
+// after istallation go to views and route these:
+// blog\resources\views\welcome.blade.php
+// blog\routes\web.php
+// -------------------
+// get:
+// ----
 Route::get('/', function () {
     return view('welcome');
 });
