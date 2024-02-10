87cb4266d046be1871a5428cc8bc2ab4250894df
digitaldeba <129753464+digitaldeba@users.noreply.github.com>
2024-02-10 15:06:39 +0530

    Update 5,6Route.php

--- blog/5,6Route.php
+++ blog/5,6Route.php
@@ -11,18 +11,77 @@
 //     return view('blade-file');
 // });
 
-Route::get('/', function () {
-    return view('welcome');
-    // return "<h1>Post Page</h1>";
-});
+// Route::get('/', function () {
+//     return view('welcome');
+//     // return "<h1>Post Page</h1>";
+// });
 
-Route::get('/post', function () {
-    return "<h1>Post Page</h1>";
-});
+// Route::get('/post', function () {
+//     return "<h1>Post Page</h1>";
+// });
 
-Route::view('/blogs','post');
+// Route::view('/blogs','post');
 
 // php artisan route:list --except-vendor [to see only outes you made]
 // php artisan route:list --path=post [to see only post routes you made]
+
+
+// ---------vid 6,7-------------
+
+// // parameter
+// Route::get('/post/{id?}', function (string $id = null) {
+
+//     if($id){
+//         return "<h1>Post Page ID : ".$id."</h1>";
+//     }else{
+//         return "<h1>NO ID FOUND</h1>";
+//     }
+
+// });
+
+
+// // multi parameter
+// Route::get('/post/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+
+//     if($id){
+//         return "<h1>Post Page ID : ".$id."</h1><h2>".$comment."</h2>";
+//     }else{
+//         return "<h1>NO ID FOUND</h1>";
+//     }
+
+// });
+
+// // parameter validation
+// Route::get('/post/{id?}', function (string $id = null) {
+
+//     if($id){
+//         return "<h1>Post Page ID : ".$id."</h1>";
+//     }else{
+//         return "<h1>NO ID FOUND</h1>";
+//     }
+
+// })->whereNumber('id');
+// })->whereAlpha('id');
+// })->whereAlphaNumeric('id');
+// })->whereIn('id', ['movie','song','painting']);
+// })->where('id', '[0-9]+');
+// })->where('id', '[a-za-Z]+');
+
+// // multi parameter val
+
+// Route::get('/post/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+
+//     if($id){
+//         return "<h1>Post Page ID : ".$id."</h1><h2>".$comment."</h2>";
+//     }else{
+//         return "<h1>NO ID FOUND</h1>";
+//     }
+
+// })->where('id', '[0-9]+')->->where('comment', '[a-za-Z]+');
+
+
+// regex: 
+// for numbers only '[0-9]+',
+// for alphabetic only '[a-za-Z]+' 
 
 ?>
