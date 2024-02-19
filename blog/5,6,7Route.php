222bea17f4bf67e4f0c111a9851ffb17d806b5bc
digitaldeba <129753464+digitaldeba@users.noreply.github.com>
2024-02-19 15:17:08 +0530

    Update 5,6,7Route.php

--- blog/5,6,7Route.php
+++ blog/5,6,7Route.php
@@ -22,8 +22,8 @@
 
 // Route::view('/blogs','post');
 
-// php artisan route:list --except-vendor [to see only outes you made]
-// php artisan route:list --path=post [to see only post routes you made]
+// php artisan route:list --except-vendor [to see only routes you made]
+// php artisan route:list --path=post [to see only post term related routes you made]
 
 
 // ---------vid 6,7-------------
