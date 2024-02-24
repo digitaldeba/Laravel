db1223301890951ff4905fb967595e6c251d9870
digitaldeba <129753464+digitaldeba@users.noreply.github.com>
2024-02-24 23:38:13 +0530

    Update 13Controller.php

--- blog/13Controller.php
+++ blog/13Controller.php
@@ -14,7 +14,7 @@
 //     }
 
 //     //
-//     public function showUser(string $id){
+//     public function showUser(string $id = null){
 //     // return view('user', ['id' => $id]);
 
 //     // if key and value is same, use compact function
@@ -31,6 +31,48 @@
 // // at user.blade.php
 // <h1>{{ !empty($id) ? $id : 'no id found' }}</h1>
 
+// -------- for group controller-----------
 
+
+//      Route::controller(PageController::class)->group(function(){
+
+//          Route::get('/', 'showHome')->name('home');
+//          Route::get('/post', 'showBlog')->name('blog');
+//          Route::get('/user/{id?}', 'showUser')->name('myUser');
+
+//      });
+
+// // at view pages change href route to relevant names'
+
+// <li class="nav-item"><a href="{{ route('blog') }}" class="navlink">Post</a></li>
+
+
+// // ---------- for single action controller (uses like constructor) ----------
+// php artisan make:controller TestingController --invokable
+
+
+// // at web.php
+// Route::get('/testing', TestingController::class);
+
+
+// // at TestingController.php
+// class TestingController extends Controller
+// {
+//     /**
+//      * Handle the incoming request.
+//      */
+//     public function __invoke()
+//     {
+//         //
+//         return view('test');
+//     }
+// }
+
+
+// // make a test.blade.php page | invoke method can be called in normal controller also
+
+
+// // -------- to see which route is targetted to which controller --------
+// php artisan route:list --except-vendor
 
 ?>
