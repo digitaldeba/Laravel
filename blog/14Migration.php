27b8d31603bdb18dbce89042f00a9695e5742751
digitaldeba <129753464+digitaldeba@users.noreply.github.com>
2024-02-25 09:32:01 +0530

    Update 14Migration.php

--- blog/14Migration.php
+++ blog/14Migration.php
@@ -1,1 +1,37 @@
+<?php
 
+// // ------------ to create migration table ------------
+// php artisan make:migration create_students_table
+
+// // that created the migrtion file
+
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('users', function (Blueprint $table) {
+
+//             $table->id();
+//             $table->string('name');
+//             $table->string('email')->unique();
+//          
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('users');
+//     }
+// };
+
+// // ------- after designing the table structure { at env file set db name } --------
+// php artisan migrate
+
+
+?>
